1b4cbb3a010b1f407fc6747c7917a7cc326b0cbe
ezequeil valdez <ronalhvaldez@gmail.com>
2023-01-26 19:33:09 -0300

    third commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let nombre = prompt("hi! whats your name?");
+let edad = Number(prompt("how old are you?"));
+
+do {
+  if (isNaN(edad)) {
+    alert("Error, please enter a valid data");
+    edad = Number(prompt("how old are you again?"));
+  } else if (edad < 18) {
+    alert("you are underage, you can't be here");
+    edad = Number(prompt("how old are you again?"));
+  } else {
+    console.log("welcome");
+  }
+} while (edad < 18 || isNaN(edad));
+
+alert("hi,  " + nombre + "! welcome to lofi");
